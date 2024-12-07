569e81ebf3a37ac63ccbbde67867919dc6e1aef3
SosninFedor <sosninfedor@gmail.com>
2024-12-07 19:48:56 +0500

    Add project files.

---
+++ Tyuiu.SosninFM.Sprint6.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿
+using Tyuiu.SosninFM.Sprint6.Task0.V17.Lib;
+
+namespace Tyuiu.SosninFM.Sprint6.Task0.V17.Test
+{
+	[TestClass]
+	public sealed class DataServiceTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			DataService ds = new DataService();
+			double res = ds.Calculate(7, 8, 9);#
+			double wait = 0;
+			Assert.AreEqual(res, wait);
+
+
+
+
+		}
+	}
+}
