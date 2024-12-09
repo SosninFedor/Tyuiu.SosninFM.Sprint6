864c4d46cb0aa2a5786be54c27d24d7157283ac2
SosninFedor <sosninfedor@gmail.com>
2024-12-10 02:51:25 +0500

    Task5

---
+++ Tyuiu.SosninFM.Sprint6.Task5.V19.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SosninFM.Sprint6.Task5.V19.Lib;
+
+namespace Tyuiu.SosninFM.Sprint6.Task5.V19.Test
+{
+	[TestClass]
+	public sealed class DataServiceTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string path = @"D:\Programs\VisualStudio\Projects\repos\2024\Tyuiu.SosninFM.Sprint6\Sprint6Task6\InPutDataFileTask6V19.txt";
+			FileInfo info = new FileInfo(path);
+			bool res = info.Exists;
+			bool wait = true;
+			Assert.AreEqual(wait, res);
+
+
+		}
+	}
+}
---
+++ Tyuiu.SosninFM.Sprint6.Task5.V19/FormMain.cs
@@ -0,0 +1,17 @@
+using Tyuiu.SosninFM.Sprint6.Task5.V19.Lib;
+
+namespace Tyuiu.SosninFM.Sprint6.Task5.V19
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
