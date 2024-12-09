02aca83710cd478b52eb8b827d24ac1da9d34bc6
SosninFedor <sosninfedor@gmail.com>
2024-12-09 14:46:28 +0500

    Task0

--- Tyuiu.SosninFM.Sprint6.Task0.V17.Lib/DataService.cs
+++ Tyuiu.SosninFM.Sprint6.Task0.V17.Lib/DataService.cs
@@ -1,5 +1,4 @@
-﻿
-using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.SosninFM.Sprint6.Task0.V17.Lib
 {
@@ -7,9 +6,9 @@ namespace Tyuiu.SosninFM.Sprint6.Task0.V17.Lib
 	{
 		public double Calculate(int x)
 		{
-
 			double res = Math.Round(Math.Exp(x) / x, 3);
 			return res;
 		}
 	}
 }
+
