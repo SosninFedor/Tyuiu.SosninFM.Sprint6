a5424ec57d60f40f43dd82492b1d9c520223f396
SosninFedor <sosninfedor@gmail.com>
2024-12-09 14:33:43 +0500

    Task0

--- Tyuiu.SosninFM.Sprint6.Task0.V17/FormMain.cs
+++ Tyuiu.SosninFM.Sprint6.Task0.V17/FormMain.cs
@@ -1,3 +1,5 @@
+using Tyuiu.SosninFM.Sprint6.Task0.V17.Lib;
+
 namespace Tyuiu.SosninFM.Sprint6.Task0.V17
 {
 	public partial class FormMain : Form
@@ -6,5 +8,62 @@ namespace Tyuiu.SosninFM.Sprint6.Task0.V17
 		{
 			InitializeComponent();
 		}
+
+		private void groupBoxCond_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void buttonDone_SAA_Click(object sender, EventArgs e)
+		{
+			DataService ds = new DataService();
+			try
+			{
+				textBoxRes_SAA.Text = Convert.ToString(ds.Calculate(Convert.ToInt32(textBoxX_SAA.Text)));
+			}
+			catch
+			{
+				MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void textBoxX_SAA_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if ((e.KeyChar <= 47 || e.KeyChar >= 58) && (e.KeyChar != ',') && (e.KeyChar != 8))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void buttonHelp_SAA_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Таск выполнил студент группы АСОиУб-24-1 Соснин Фёдор Михайлович", "Cообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void textBoxX_SAA_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void groupBoxInPutData_SAA_Enter(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
+
