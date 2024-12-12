4733a8b90f18a505083edfbb8766bc9c367c25c6
SosninFedor <sosninfedor@gmail.com>
2024-12-12 21:42:55 +0500

    Task6

---
+++ Tyuiu.SosninFM.Sprint6.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SosninFM.Sprint6.Task6.V20.Lib;
+
+namespace Tyuiu.SosninFM.Sprint6.Task6.V20.Test
+{
+	[TestClass]
+	public sealed class DataServiceTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			DataService ds = new DataService();
+			string path = @"D:\Programs\VisualStudio\Projects\repos\2024\Tyuiu.SosninFM.Sprint6\Tyuiu.SosninFM.Sprint6.Task6.V20\bin\Debug\InPutDataFileTask6V20.txt";
+			string res = ds.CollectTextFromFile(path);
+			string wait = "dKOPBC IqjpIlKAaSCBuaQUU GPeEqZo FPsrQHsImzbTBckw NgAfitECn";
+			Assert.AreEqual(wait, res);
+		}
+	}
+}
---
+++ Tyuiu.SosninFM.Sprint6.Task6.V20/FormMain.cs
@@ -0,0 +1,13 @@
+
+using Tyuiu.SosninFM.Sprint6.Task6.V20.Lib;
+
+namespace Tyuiu.SosninFM.Sprint6.Task6.V20
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+	}
+}
