733e39a5260851e71a584f06be984d0244fbcc7e
SosninFedor <sosninfedor@gmail.com>
2024-12-12 21:56:01 +0500

    Task6

--- Tyuiu.SosninFM.Sprint6.Task6.V20/FormMain.cs
+++ Tyuiu.SosninFM.Sprint6.Task6.V20/FormMain.cs
@@ -1,4 +1,5 @@
 
+using System.Windows.Forms;
 using Tyuiu.SosninFM.Sprint6.Task6.V20.Lib;
 
 namespace Tyuiu.SosninFM.Sprint6.Task6.V20
@@ -9,5 +10,37 @@ namespace Tyuiu.SosninFM.Sprint6.Task6.V20
 		{
 			InitializeComponent();
 		}
+		{
+    public partial class FormMain : Form
+		{
+			public FormMain()
+			{
+				InitializeComponent();
+			}
+			DataService ds = new DataService();
+			private void button_Imput_Click(object sender, EventArgs e)
+			{
+				openFileDialog1.ShowDialog();
+				string fileName = openFileDialog1.FileName;
+				openFilePath.Text = fileName;
+				textBox_Imput.Text = File.ReadAllText(fileName);
+				button_Go.Enabled = true;
+
+			}
+
+			private void button_Go_Click(object sender, EventArgs e)
+			{
+				string str = " ";
+				string path = Convert.ToString(openFilePath.Text);
+				textBox_Res.Text = ds.CollectTextFromFile(path);
+			}
+
+			private void button_Spr_Click(object sender, EventArgs e)
+			{
+				About formAbout = new About();
+				formAbout.ShowDialog();
+			}
+		}
 	}
+}
 }
