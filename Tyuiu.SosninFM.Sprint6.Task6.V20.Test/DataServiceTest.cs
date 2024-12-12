8fd160991ce6a668a1584ef0d5f652aa42cd7982
SosninFedor <sosninfedor@gmail.com>
2024-12-12 21:46:12 +0500

    Task6

--- Tyuiu.SosninFM.Sprint6.Task6.V20.Test/DataServiceTest.cs
+++ Tyuiu.SosninFM.Sprint6.Task6.V20.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.SosninFM.Sprint6.Task6.V20.Test
 			DataService ds = new DataService();
 			string path = @"D:\Programs\VisualStudio\Projects\repos\2024\Tyuiu.SosninFM.Sprint6\Tyuiu.SosninFM.Sprint6.Task6.V20\bin\Debug\InPutDataFileTask6V20.txt";
 			string res = ds.CollectTextFromFile(path);
-			string wait = "dKOPBC IqjpIlKAaSCBuaQUU GPeEqZo FPsrQHsImzbTBckw NgAfitECn";
+			string wait = "dKOPBC IqjpIlKAaSCBuaQUU JPPzrPC FPsrQHsImzbTBckw NgAfitECn";
 			Assert.AreEqual(wait, res);
 		}
 	}
